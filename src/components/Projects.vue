c2326372a9cb7a70dca397f142b5ac3deeb9cb7b
crooksryan <rdc5410@psu.edu>
2023-08-29 19:13:13 -0400

    styled buttons and added animation to the projects section

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -4,132 +4,166 @@ import { ref } from 'vue';
 
 const index = ref(0);
 
+function buttonHandler(val, event) {
+    index.value = val;
+
+    document.querySelectorAll('.selected').forEach(element => {
+        element.classList.remove('selected')
+    })
+
+    event.target.classList.add('selected');
+}
+
+function buttonWrapper(event) {
+
+}
+
 </script>
 
 <template>
     <div class="section">
         <h1>Projects</h1>
 
-        <button @click="index=0">Python</button>
-        <button @click="index=1">Javascript</button>
-        <button @click="index=2">Other</button>
-
-
-        <div class="language" v-if='index == 0'>
-            <h2>Python</h2>
-            <div class="project-container" >
-                <div class="project" >
-                    <h3>Paper-Trader</h3>
-
-                    <p>
-                        Built with Flask, PostgreSQL, and the Alpaca Trading API.
-                        Allows for users to make accounts and make stocks trades with paper money.
-                    </p>
-
-                    <a href="">Github >></a>
-                </div>
-                <div class="project">
-                    <h3>Data Mine</h3>
-
-                    <p>
-                        Runs with scheduled events to gather stock data, make ML models, makes predictions,
-                        and manages a portfolio. Then uses a Flask app to host a dashboard to track progress.
-                    </p>
-
-                    <a href="">Github >></a>
-                </div>
-                <div class="project">
-                    <h3>Pi-Daily Email</h3>
-
-                    <p>
-                        Uses multiple APIs to format an email every day to send useful information to a list of users.
-                    </p>
-
-                    <a href="">Github >></a>
-                </div>
-                <div class="project">
-                    <h3>Social Media App</h3>
-
-                    <p>
-                        Built using Django. Users can make accounts, post, follow users, see global feeds, and followed feed.
-                    </p>
-
-                    <a href="">Github >></a>
-                </div>
-                <div class="project">
-                    <h3>Chat App</h3>
-
-                    <p>Uses a Flask app and SocketIO for users to send group messages in real time.</p>
-
-                    <a href="">Github >></a>
-                </div>
-            </div>
+        <div class="button-holder">
+            <button @click="buttonHandler(0, $event)" class="selected">Python</button>
+            <button @click="buttonHandler(1, $event)">Javascript</button>
+            <button @click="buttonHandler(2, $event)">Other</button>
         </div>
 
-        <div class="language" v-if='index == 1'>
-            <h2>Javascript</h2>
-            <div class="project-container">
-                <div class="project">
-                    <h3>Paper-Trader Front End</h3>
+        <Transition name="fade">
+            <div class="language" v-if='index == 0'>
+                <h2>Python</h2>
+                <div class="project-container">
+                    <div class="project">
+                        <h3>Paper-Trader Back End</h3>
 
-                    Project description
+                        <p>
+                            Built with Flask, PostgreSQL, SocketIO, and the Alpaca Trading API.
+                            Allows for users to make accounts and make stocks trades with paper money.
+                        </p>
 
-                    <br>
+                        <a href="">Github >></a>
+                    </div>
+                    <div class="project">
+                        <h3>Data Mine</h3>
 
-                    <a href="">Github >></a>
-                </div>
-                <div class="project">
-                    <h3>Chat App</h3>
+                        <p>
+                            Runs with scheduled events to gather stock data, make ML models, makes predictions,
+                            and manages a portfolio. Then uses a Flask app to host a dashboard to track progress.
+                        </p>
 
-                    Project description
+                        <a href="">Github >></a>
+                    </div>
+                    <div class="project">
+                        <h3>Pi-Daily Email</h3>
 
-                    <br>
+                        <p>
+                            Uses multiple APIs to format an email every day to send useful information to a list of users.
+                        </p>
 
-                    <a href="">Github >></a>
-                </div>
-                <div class="project">
-                    <h3>Blockchain Coin-Flip</h3>
+                        <a href="">Github >></a>
+                    </div>
+                    <div class="project">
+                        <h3>Social Media App</h3>
 
-                    Project description
+                        <p>
+                            Built using Django. Users can make accounts, post, follow users, see global feeds, and followed
+                            feed.
+                        </p>
 
-                    <br>
+                        <a href="">Github >></a>
+                    </div>
+                    <div class="project">
+                        <h3>Chat App</h3>
 
-                    <a href="">Github >></a>
-                </div>
-                <div class="project">
-                    <h3>This Website</h3>
+                        <p>
+                            Uses a Flask app and SocketIO for users to send group messages in real time.
+                        </p>
 
-                    Project description
-
-                    <br>
-
-                    <a href="">Github >></a>
-                </div>
-            </div>
-        </div>
-
-        <div class="language" v-if='index == 2'>
-            <h2>Other</h2>
-            <div class="project-container">
-                <div class="project">
-                    <h3>Course Scheduler</h3>
-
-                    <p>Made with Java and SQL.</p>
-                    
-
-                    <a href="">Github >></a>
-                </div>
-                <div class="project">
-                    <h3>Language Learning</h3>
-
-                    <p>Currently leanring Rust.</p>
-
-                    <br>
-
-                    <a href="">Github >></a>
+                        <a href="">Github >></a>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Transition>
+
+        <Transition name="fade">
+
+            <div class="language" v-if='index == 1'>
+                <h2>Javascript</h2>
+                <div class="project-container">
+                    <div class="project">
+                        <h3>Paper-Trader Front End</h3>
+
+                        <p>
+                            Front end was built using SocketIO to update prices when the user interacts with the app.
+                            Used JQuery on some pages to simplify development process.
+                        </p>
+
+                        <a href="">Github >></a>
+                    </div>
+                    <div class="project">
+                        <h3>Blockchain Coin-Flip</h3>
+
+                        <p>
+                            Project built with Vue.JS. Allows for users to sync MetaMask wallets and then wager on a coin
+                            flip.
+                            It is a winner take all. Smart contract was made with Solidity.
+                        </p>
+
+                        <a href="">Github >></a>
+                    </div>
+                    <div class="project">
+                        <h3>Chat App</h3>
+
+                        <p>
+                            Live time chat app. Front end uses SocketIO to handle all events.
+                            Then uses vanilla JS to manipulate the DOM.
+                        </p>
+
+                        <a href="">Github >></a>
+                    </div>
+                    <div class="project">
+                        <h3>This Website</h3>
+
+                        <p>
+                            This site is built using Vue.JS and hosted on Firebase and utilizes other Firebase tools.
+                        </p>
+
+                        <a href="">Github >></a>
+                    </div>
+                </div>
+            </div>
+        </Transition>
+
+        <Transition name="fade">
+
+            <div class="language" v-if='index == 2'>
+                <h2>Other</h2>
+                <div class="project-container">
+                    <div class="project">
+                        <h3>Course Scheduler</h3>
+
+                        <p>
+                            Made with Java and SQL. Students can register for classes and view schedules.
+                            Admins can register students and classes and view student schedules.
+                        </p>
+
+
+                        <a href="">Github >></a>
+                    </div>
+                    <div class="project">
+                        <h3>Language Learning</h3>
+
+                        <p>
+                            Repository to track progress when learning a new language.
+                            Currently leanring Rust. Planning on learngin Go and Zig.
+                        </p>
+
+                        <a href="">Github >></a>
+                    </div>
+                </div>
+            </div>
+        </Transition>
 
     </div>
 </template>
@@ -160,17 +194,17 @@ h1 {
     transition: all 350ms;
 }
 
-.project h3{
+.project h3 {
     margin: 0.5rem 0;
 }
 
 
 @media only screen and (min-width: 650px) {
-    .project{
+    .project {
         width: 15rem;
     }
 
-    .project-container:hover .project:hover{
+    .project-container:hover .project:hover {
         filter: brightness(100%) grayscale(0);
         background-color: var(--secondary);
         box-shadow: 0 0 0 0;
@@ -179,18 +213,18 @@ h1 {
     }
 
     /* .project-container:hover .project{ */
-        /* filter: brightness(45%) grayscale(100%); */
-        /* box-shadow: 0 0 4px 1px #888888;  */
-        /* scale: 90%;  */
+    /* filter: brightness(45%) grayscale(100%); */
+    /* box-shadow: 0 0 4px 1px #888888;  */
+    /* scale: 90%;  */
     /* } */
 }
 
 @media only screen and (max-width: 300px) {
-    .project-container{
+    .project-container {
         flex-direction: column;
     }
 
-    .project{
+    .project {
         margin: 1rem 0;
     }
 }
@@ -244,5 +278,93 @@ a:hover:before {
     }
 }
 
+.button-holder {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
 
-</style>
+button {
+    flex: 1;
+    position: relative;
+    padding: 1rem 2rem;
+    /* border: 2px solid #f2f2f2; */
+    border: none;
+    text-transform: uppercase;
+    font-weight: bold;
+    letter-spacing: 1px;
+    box-shadow: 1px 1px 20px rgba(0, 0, 0, 0.1);
+    color: var(--text);
+    background: transparent;
+    overflow: hidden;
+    transition: all 350ms;
+}
+
+button::before,
+button::after {
+    position: absolute;
+    content: "";
+    left: 0;
+    right: 0;
+    bottom: 0;
+}
+
+button::after {
+    height: 0;
+    background-color: var(--accent);
+    transition: height 500ms ease;
+    z-index: -1;
+}
+
+button::before {
+    background-color: var(--secondary);
+    height: 100%;
+    z-index: -2;
+}
+
+button:hover {
+    color: var(--background);
+    transition: 0.5s ease;
+}
+
+button:hover::after {
+    height: 100%;
+}
+
+button:nth-of-type(1) {
+    border-radius: 6px 0 0 6px;
+    border-top: 2px solid var(--primary);
+    border-left: 2px solid var(--primary);
+    border-bottom: 2px solid var(--primary);
+}
+
+button:nth-of-type(2) {
+    border: 2px solid var(--primary);
+    /* border-left: 2px solid var(--primary); */
+}
+
+button:nth-of-type(3) {
+    border-radius: 0 6px 6px 0;
+    border-top: 2px solid var(--primary);
+    border-right: 2px solid var(--primary);
+    border-bottom: 2px solid var(--primary);
+}
+
+.selected {
+    background-color: var(--accent);
+    color: var(--background);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 350ms;
+}
+
+.fade-enter-active{
+    transition-delay: 200ms;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}</style>
